254bf88c4ba7fbbf49fa7db71a80c2c0a8edcfbe
jsaless <joaovssales@gmail.com>
2024-04-29 00:04:51 -0300

    feat: define constants data

---
+++ modules/common/constants.ts
@@ -0,0 +1,14 @@
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+
+const config = new pulumi.Config();
+
+// Stack Configs
+export const projectName = pulumi.getProject();
+export const environment = pulumi.getStack();
+
+// AWS Configs
+export const awsAccountId = pulumi.output(aws.getCallerIdentity({}).then((caller) => caller.accountId));
+export const awsRegion = aws.getRegionOutput({}).name;
+export const policyVersion = "2012-10-17";
+
