48078835eff53a6510fa0abd370d616b23752a2b
jsaless <joaovssales@gmail.com>
2024-05-19 00:08:46 -0300

    feat: adding custom names to each resource and environment variables

--- index.ts
+++ index.ts
@@ -1,121 +1,132 @@
+// ACM
 import CertificateBuilder from "./src/acm/certificate/builder";
 import CertificateValidationBuilder from "./src/acm/certificate-validation/builder";
 
+// CloudFront
 import { DistributionBuilder, ViewerCertificateBuilder, OriginsBuilder, DefaultCacheBehaviorBuilder } from "./src/cloudfront/distribution/builder";
 import OriginAccessControlBuilder from "./src/cloudfront/origin-access-control/builder";
 
+// Route53
 import RegisteredDomainBuilder from "./src/route53/domain/builder";
-import { RecordAliasesBuilder, RecordBuilder} from "./src/route53/record/builder";
+import { RecordAliasesBuilder, RecordBuilder } from "./src/route53/record/builder";
 import ZoneBuilder from "./src/route53/zone/builder";
 
+// S3
 import BucketBuilder from "./src/s3/bucket/builder";
 import BucketObjectBuilder from "./src/s3/object/builder";
 import BucketPolicyBuilder from "./src/s3/policy/builder";
-import { bucketDomainName, bucketPolicy } from "./modules/common/constants"
 
-module.exports = async() => {
+// Constants & Names
+import * as consts from "./modules/common/constants"
+import { interpolate } from "@pulumi/pulumi";
+
+module.exports = () => {
     const hostedZone = new ZoneBuilder()
-        .setPulumiName("public-zone")
-        .setName(bucketDomainName)
+        .setPulumiName(consts.hostedZoneName)
+        .setName(consts.bucketDomainName)
         .build();
-    
+
     new RegisteredDomainBuilder()
-        .setPulumiName("my-domain")
-        .setDomainName(bucketDomainName)
+        .setPulumiName(consts.domainName)
+        .setDomainName(consts.bucketDomainName)
         .setNameServers(
             hostedZone.getAwsComponent().nameServers.apply(servers => {
-                return servers.map(server => ({name: server}));
+                return servers.map(server => ({ name: server }));
             })
         )
         .build();
-    
+
     const acmCertificate = new CertificateBuilder()
-        .setPulumiName("acm-certificate")
-        .setDomainName(bucketDomainName)
+        .setPulumiName(consts.certificateName)
+        .setDomainName(consts.bucketDomainName)
         .setValidationMethod("DNS")
         .build();
-    
+
     new RecordBuilder()
-        .setPulumiName("validation-records")
+        .setPulumiName(consts.validationRecordsName)
         .setZoneId(hostedZone.getAwsComponent().id)
         .setTtl(300)
         .setName(acmCertificate.getAwsComponent().domainValidationOptions[0].resourceRecordName)
         .setType(acmCertificate.getAwsComponent().domainValidationOptions[0].resourceRecordType)
         .setRecords([acmCertificate.getAwsComponent().domainValidationOptions[0].resourceRecordValue])
         .build();
-    
+
     const validateCertificate = new CertificateValidationBuilder()
-        .setPulumiName("certificate-validation")
+        .setPulumiName(consts.certificateValidationName)
         .setCertificateValidation(acmCertificate.getAwsComponent().arn)
         .build();
-    
+
     const bucket = new BucketBuilder()
-        .setPulumiName("bucket")
-        .setBucket(bucketDomainName)
+        .setPulumiName(consts.bucketName)
+        .setBucket(consts.bucketDomainName)
         .build();
-    
+
     new BucketObjectBuilder()
-        .setPulumiName("bucket-object")
-        .setKey("index.html")
+        .setPulumiName(consts.bucketObjectName)
+        .setKey(consts.localFileName)
         .setContentType("text/html")
         .setBucket(bucket.getAwsComponent().id)
-        .setSource("./modules/components/index.html")
+        .setSource(consts.localFilePath)
         .build();
-    
+
     const originAccessControl = new OriginAccessControlBuilder()
-        .setPulumiName("origin-access-control")
+        .setPulumiName(consts.originAccessControlName)
         .setName(bucket.getAwsComponent().bucketRegionalDomainName)
         .setOriginAccessControlOriginType("s3")
         .setSigningBehavior("always")
         .setSigningProtocol("sigv4")
         .build();
-    
+
     // CloudFront
     const origins = new OriginsBuilder()
         .setDomainName(bucket.getAwsComponent().bucketRegionalDomainName)
         .setOriginId(bucket.getAwsComponent().id)
         .setOriginAccessControlId(originAccessControl.getAwsComponent().id)
         .build();
-    
+
     const viewerCertificate = new ViewerCertificateBuilder()
         .setAcmCertificateArn(acmCertificate.getAwsComponent().arn)
         .build();
-    
+
     const defaultCacheBehavior = new DefaultCacheBehaviorBuilder()
         .setAllowedMethods(["GET", "HEAD"])
         .setCachedMethods(["GET", "HEAD"])
         .setTargetOriginId(bucket.getAwsComponent().id)
         .setViewerProtocolPolicy("redirect-to-https")
         .build();
-    
+
     const distribution = new DistributionBuilder()
-        .setPulumiName("distribution")
+        .setPulumiName(consts.distributionName)
         .setOrigins(origins)
-        .setAliases([bucketDomainName])
+        .setAliases([consts.bucketDomainName])
         .setPriceClass("PriceClass_100")
-        .setDefaultRootObject("index.html")
+        .setDefaultRootObject(consts.localFileName)
         .setViewerCertificate(viewerCertificate)
         .setDefaultCacheBehavior(defaultCacheBehavior)
         .setDependsOn(validateCertificate.getAwsComponent())
         .build();
-    
+
     new BucketPolicyBuilder()
-        .setPulumiName("bucket-policy")
+        .setPulumiName(consts.bucketPolicyName)
         .setBucket(bucket.getAwsComponent().bucket)
-        .setPolicy(bucketPolicy(distribution.getAwsComponent().id))
+        .setPolicy(consts.bucketPolicy(distribution.getAwsComponent().id))
         .build();
-    
+
     const recordAliases = new RecordAliasesBuilder()
         .setName(distribution.getAwsComponent().domainName)
         .setZoneId(distribution.getAwsComponent().hostedZoneId)
         .setEvaluateTargetHealth(false)
         .build();
-    
+
     new RecordBuilder()
-        .setPulumiName("records-to-distribution")
+        .setPulumiName(consts.distributionRecordsName)
         .setZoneId(hostedZone.getAwsComponent().id)
         .setAliases(recordAliases)
-        .setName(bucketDomainName)
+        .setName(consts.bucketDomainName)
         .setType("A")
         .build();
+
+    return {
+        cloudfrontUrl: interpolate`https://${distribution.getAwsComponent().aliases}`
+    };
 };
--- modules/common/constants.ts
+++ modules/common/constants.ts
@@ -1,11 +1,8 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
+import n from "@soufantech/pulumi-naming";
 
 const config = new pulumi.Config();
-
-// Stack Configs
-export const projectName = pulumi.getProject();
-export const environment = pulumi.getStack();
 
 // AWS Configs
 export const awsAccountId = pulumi.output(aws.getCallerIdentity({}).then((caller) => caller.accountId));
@@ -14,6 +11,21 @@ export const policyVersion = "2012-10-17";
 
 // Project Configs
 export const bucketDomainName = config.require("bucket-domain-name");
+export const localFilePath = config.require("local-file-path");
+export const localFileName = config.require("local-file-name");
+
+// Resource Names
+export const hostedZoneName = n("zone");
+export const domainName = n("domain");
+export const certificateName = n("certificate");
+export const validationRecordsName = n("validation-records");
+export const certificateValidationName = n("certificate-validation");
+export const bucketName = n("bucket");
+export const bucketObjectName = n("object");
+export const originAccessControlName = n("origin-access-control");
+export const bucketPolicyName = n("policy");
+export const distributionName = n("distribution");
+export const distributionRecordsName = n("distribution-records");
 
 // Policies
 export const bucketPolicy = (distribution: pulumi.Input<string>) => aws.iam.getPolicyDocumentOutput({
